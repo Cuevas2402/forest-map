5c69d6b1a025d7e9f98f79937ef6e8398c36fe28
Juan Cuevas <juan.m.cuevas@outlook.com>
2024-09-29 12:19:47 -0600

    App: Modify navbar

--- application/src/components/NavBar.tsx
+++ application/src/components/NavBar.tsx
@@ -1,21 +1,28 @@
 
 import {
-  Bell,
-  Trees
+	Bell,
+	LineChart,
+	Package,
+	Users,
+	TreeDeciduous,
+	Trees,
+	Code2,
+	SquareTerminal
 } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
-import NavLink from "./NavLink"
+import { Link, useLocation } from "react-router-dom"
 
 export default function NavBar(){
 
+	const location = useLocation();
+	const path : string = location.pathname
 
-	const tabs = [
-		{icon: "package", label : "Dashboard", href: "/" , isactive : true},
-		{icon: "trees", label : "Trees", href: "/trees" , isactive : false},
-		{icon: "users", label : "Users", href: "/users" , isactive : false},
-		{icon: "linechart", label : "Analytics", href: "a" , isactive : false},
-	]
+	const csscl : string = "w-full flex items-center gap-3 rounded-lg px-3 py-2 transition-all "
+
+	const isactive : string = csscl + " bg-black text-white  hover:text-neutral ";
+	const noactive : string = csscl + " hover:text-white hover:bg-black ";
+
 	return (
 		<>
 			<div className="min-h-screen hidden border-r bg-muted/40 md:block">
@@ -32,11 +39,50 @@ export default function NavBar(){
 					</div>
 					<div className="flex-1">
 						<nav className="flex flex-col gap-y-2 items-start px-2 text-sm font-medium lg:px-4">
-							{
-								tabs.map((item) => (
-									<NavLink {...item}/>
-								))
-							}
+
+							<Link to="/" className={path == "/" ? isactive : noactive}>
+								<Package className="h-4 w-4" />
+								Dashboard
+							
+							</Link>
+
+							<Link to="/trees" className={path.indexOf("trees") != -1 ? isactive : noactive}>
+
+
+								<TreeDeciduous className="h-4 w-4"/>
+								Trees
+							
+							</Link>
+
+							<Link to="/users" className={path.indexOf("users") != -1 ? isactive : noactive}>
+								<Users className="h-4 w-4" />
+								Users
+							
+							</Link>
+
+							<Link to="#" className={noactive}>
+
+								<LineChart className="h-4 w-4" />
+								Analytics
+							
+							</Link>
+
+							<Link to="#" className={noactive}>
+
+								<SquareTerminal className="h-4 w-4" />
+
+								Playground
+
+							</Link>
+
+							<Link to="#" className={noactive}>
+
+								<Code2 className="h-4 w-4" />
+
+								Models
+							
+							</Link>
+
 						</nav>
 					</div>
 				</div>
