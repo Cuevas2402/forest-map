1bbfdc0d595018a83e7e63e0434f9eb23e98b45a
Juan Cuevas <juan.m.cuevas@outlook.com>
2024-12-01 12:28:16 -0600

    App: Display zone map

--- application/src/views/forest/tabs/Zones/parts/ZoneImages.tsx
+++ application/src/views/forest/tabs/Zones/parts/ZoneImages.tsx
@@ -1,22 +1,47 @@
-import { Card, CardContent, CardHeader} from "@/components/ui/card";
-import fondologin from "../../../../../assets/fondologin.jpg"
+import { Card, CardContent } from "@/components/ui/card";
+import { MapContainer, ImageOverlay, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import fondologin from "../../../../../assets/fondologin.jpg";
 
-export default function ZoneImages () {
-	return (
-		<Card className="overflow-hidden h-full pt-5" x-chunk="dashboard-07-chunk-4">
+export default function ZoneImages() {
+  const bounds : any = [[-50, -50], [150, 200]]; // 3:2 ajustado
 
-			<CardContent>
-				<div className="grid gap-2">
 
-					<img
-					alt="Product image"
-					className="mt-3 aspect-square h-full w-full rounded-md object-cover"
-					height="300"
-					src={fondologin}
-					width="300"
-					/>
-				</div>
-			</CardContent>
-		</Card>
-	)
+  return (
+    <Card className="overflow-hidden h-full pt-5" x-chunk="dashboard-07-chunk-4">
+      <CardContent>
+        <div className="grid gap-2">
+          <MapContainer
+            bounds={bounds}    // Configura los límites del mapa
+            zoom={130}
+			style={{ width: "100%", height: "90vh" }}
+          >
+			<Marker position={[0,0]}>
+				<Popup>
+					Pine	
+				</Popup>
+			</Marker>
+
+			<Marker position={[50,50]}>
+				<Popup>
+					Birch
+				</Popup>
+			</Marker>
+
+			<Marker position={[60,10]}>
+				<Popup>
+					Birch
+				</Popup>
+			</Marker>
+
+
+            <ImageOverlay
+              url={fondologin} // Ruta de la imagen que quieres mostrar
+              bounds={bounds} // Esto hará que la imagen ocupe todo el área del mapa
+            />
+          </MapContainer>
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
